2599283c71975afb01a34b7037e3b6869d513e02
James Reeves <jreeves@weavejester.com>
2011-10-01 22:29:14 +0100

    Added a test to check dependant files are reloaded

--- test/ns_tracker/test/core.clj
+++ test/ns_tracker/test/core.clj
@@ -8,9 +8,20 @@
   (.mkdirs (file "tmp/example"))
   (try
     (let [modified-namespaces (ns-tracker [(file "tmp")])]
-      (Thread/sleep 1000)
-      (spit (file "tmp/example/core.clj") '(ns example.core))
-      (is (= (modified-namespaces) #{'example.core}))
-      (is (empty? (modified-namespaces))))
+      (testing "modified files are reloaded"
+        (Thread/sleep 1000)
+        (spit (file "tmp/example/core.clj") '(ns example.core))
+        (is (= (modified-namespaces) #{'example.core}))
+        (is (empty? (modified-namespaces))))
+      
+      (testing "dependant files are reloaded"
+        (Thread/sleep 1000)
+        (spit (file "tmp/example/util.clj") '(ns example.util))
+        (spit (file "tmp/example/core.clj")
+              '(ns example.core (:use example.util)))
+        (modified-namespaces)
+        (Thread/sleep 1000)
+        (spit (file "tmp/example/util.clj") '(ns example.util))
+        (is (= (modified-namespaces) #{'example.core 'example.util}))))
     (finally
      (FileUtils/deleteDirectory (file "tmp")))))
