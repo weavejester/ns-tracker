efd5d7b26b0d6b533f690cb32db0e59b18885c3a
Deon Moolman <cmdrdats@gmail.com>
2012-08-16 18:26:00 +0200

    Add a quick way to fire off a background nstracker thread, calling a passed function with changed namespaces after reloading

--- src/ns_tracker/core.clj
+++ src/ns_tracker/core.clj
@@ -72,3 +72,28 @@
                (reset! timestamp now)
                (swap! dependency-graph update-dependency-graph new-decls)
                affected-names)))))))
+
+
+(defn check-namespace-changes
+  "Use a tracker to check for namespace changes, reloading them and calls reload-fn if there were any namespaces, passing a list of them as its single arg"
+  [track delay reload-fn]
+  (try
+    (if-let [t (track)]
+      (when t
+        (doseq [ns-sym t]
+          (require ns-sym :reload))
+        (when reload-fn
+          (reload-fn t))))
+    (catch Throwable e (.printStackTrace e)))
+  (Thread/sleep (or delay 500)))
+
+(defn start-nstracker
+  "Fire off a thread to keep checking for namespaces that need reloading. Optionally provide a delay in ms between checks - defaults to 500ms"
+  [dirs & [delay reload-fn]]
+  (let [track (ns-tracker dirs)]
+    (doto
+        (Thread.
+         #(while true
+            (check-namespace-changes track delay reload-fn)))
+      (.setDaemon true)
+      (.start))))
