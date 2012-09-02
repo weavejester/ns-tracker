f186ca34969aa01eeee1081c3ed1b8b48ff7e778
James Reeves <jreeves@weavejester.com>
2012-09-02 14:45:55 +0100

    Updated dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject ns-tracker "0.1.1"
   :description "Keep track of which namespaces have been modified"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/tools.namespace "0.1.0"]]
+                 [org.clojure/tools.namespace "0.1.3"]
+                 [org.clojure/java.classpath "0.2.0"]]
   :profiles
   {:dev {:dependencies [[commons-io "1.4"]]}})
