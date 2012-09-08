aa7a56c00bd44004e085d1e3487c4ce0e51e8fc8
James Reeves <jreeves@weavejester.com>
2012-09-09 00:57:14 +0100

    Added profiles for running under Clojure 1.3 and 1.4

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
                  [org.clojure/tools.namespace "0.1.3"]
                  [org.clojure/java.classpath "0.2.0"]]
   :profiles
-  {:dev {:dependencies [[commons-io "1.4"]]}})
+  {:dev {:dependencies [[commons-io "1.4"]]}
+   :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}})
