5b3d9ff88a665b3ec17a8b20015d9d2c0c571ada
hutch <hutch@recursive.ca>
2012-12-28 14:11:55 -0500

    -- will now re-load namespaces if a source file that is loaded changes.    This will only work if the first non-comment form of the 'helper' or    'extension' source file is an (in-ns) form. It will not look for any    subsequent in-ns forms in the file. This is the same idiomatic usage of    load/in-ns that is found in clojure/core. -- newer-namespace-decls returns a list of ns-decls (as before) but also    a set of namespace names (symbols) that have been affected. -- avoid calling .fileModifed twice -- replace contains? (as in    https://github.com/ahjones/ns-tracker/commit/164822959e47c2d8d2540aa063140f2a9609bfbc) -- use tools.namespace 0.2.2 (similarly to    https://github.com/jakemcc/ns-tracker/commit/6884b4cd8ad6e8547ce5ed6f54e3f257c547d215) -- use clojure 1.3.0 (required by tools.namespace)

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject ns-tracker "0.2.0"
   :description "Keep track of which namespaces have been modified"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/tools.namespace "0.1.3"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/tools.namespace "0.2.2"]
                  [org.clojure/java.classpath "0.2.0"]]
   :profiles
   {:dev {:dependencies [[commons-io "1.4"]]}
