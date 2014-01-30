955543fb07854b93bbfe051d4648c7258c1b50b8
James Reeves <jreeves@weavejester.com>
2014-01-30 23:00:38 +0000

    Released 0.2.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ns-tracker "0.2.1"
+(defproject ns-tracker "0.2.2"
   :description "Keep track of which namespaces have been modified"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.namespace "0.2.4"]
