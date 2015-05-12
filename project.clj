7bc2fb4d15d52d12bd031f66345f1693b9a5caed
James Reeves <jreeves@weavejester.com>
2015-05-12 19:20:09 +0100

    Update tools.namespace to 0.2.10

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject ns-tracker "0.2.2"
   :description "Keep track of which namespaces have been modified"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.namespace "0.2.4"]
+                 [org.clojure/tools.namespace "0.2.10"]
                  [org.clojure/java.classpath "0.2.2"]]
   :profiles
   {:dev {:dependencies [[commons-io "1.4"]]}
