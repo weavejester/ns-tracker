bc0b524d4d213b1c3f5d553f373358299193957b
James Reeves <jreeves@weavejester.com>
2015-05-12 17:53:59 +0100

    Add test profiles for Clojure 1.6 and 1.7

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
                  [org.clojure/java.classpath "0.2.2"]]
   :profiles
   {:dev {:dependencies [[commons-io "1.4"]]}
-   :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
    :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
+   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+   :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta2"]]}})
