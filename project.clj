24e2a87bd2aacfa7382ae3a1a5be039d26601194
James Reeves <jreeves@weavejester.com>
2015-05-12 19:14:37 +0100

    Update minimum Clojure version to 1.5.1

--- project.clj
+++ project.clj
@@ -1,11 +1,9 @@
 (defproject ns-tracker "0.2.2"
   :description "Keep track of which namespaces have been modified"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.namespace "0.2.4"]
                  [org.clojure/java.classpath "0.2.2"]]
   :profiles
   {:dev {:dependencies [[commons-io "1.4"]]}
-   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
    :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
    :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta2"]]}})
