d1859f24a3152fd3f32596873d648f5ec3802b5c
Juha Jokimäki <juha.jokimaki@iki.fi>
2016-10-24 08:06:20 +0300

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject ns-tracker "0.3.0"
   :description "Keep track of which namespaces have been modified"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.namespace "0.2.10"]
-                 [org.clojure/java.classpath "0.2.2"]]
+                 [org.clojure/tools.namespace "0.2.11"]
+                 [org.clojure/java.classpath "0.2.3"]]
   :profiles
-  {:dev {:dependencies [[commons-io "1.4"]]}
+  {:dev {:dependencies [[commons-io "2.5"]]}
    :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
    :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta2"]]}})
