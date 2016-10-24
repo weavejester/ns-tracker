8dc8f7619a9efc7835465a1b4687edcbd2a697a5
Juha Jokimäki <juha.jokimaki@iki.fi>
2016-10-24 08:08:18 +0300

    Add test profiles for Clojure 1.8 and 1.9

--- project.clj
+++ project.clj
@@ -6,4 +6,6 @@
   :profiles
   {:dev {:dependencies [[commons-io "2.5"]]}
    :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-   :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta2"]]}})
+   :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+   :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha13"]]}})
