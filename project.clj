bd1093db7d38d99496bdaabe40ae37dffd12e471
James Reeves <jreeves@weavejester.com>
2019-06-04 21:00:23 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :description "Keep track of which namespaces have been modified"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [org.clojure/java.classpath "0.2.3"]]
+                 [org.clojure/java.classpath "0.3.0"]]
   :aliases
   {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8:+1.9:+1.10" "test"]}
   :profiles
-  {:dev  {:dependencies [[commons-io "2.5"]]}
+  {:dev  {:dependencies [[commons-io "2.6"]]}
    :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
    :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
    :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
