157a3aa5e30d0b928afc265229d1ac1ee19d4503
James Reeves <jreeves@weavejester.com>
2024-06-19 17:20:08 +0100

    Add test profiles for Clojure 1.11 and 1.12

--- project.clj
+++ project.clj
@@ -7,7 +7,9 @@
                  [org.clojure/tools.namespace "1.1.0"]
                  [org.clojure/java.classpath "1.0.0"]]
   :aliases
-  {"test-all" ["with-profile" "default:+1.10" "test"]}
+  {"test-all" ["with-profile" "default:+1.10:+1.11:+1.12" "test"]}
   :profiles
   {:dev  {:dependencies [[commons-io "2.6"]]}
-   :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}})
+   :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}
+   :1.11 {:dependencies [[org.clojure/clojure "1.11.3"]]}
+   :1.12 {:dependencies [[org.clojure/clojure "1.12.0-beta1"]]}})
