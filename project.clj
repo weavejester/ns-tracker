d2474b63689d615a174f17b94d3c0fd9ece40245
James Reeves <jreeves@weavejester.com>
2024-06-19 17:17:02 +0100

    Update minimum Clojure version to 1.9.0

--- project.clj
+++ project.clj
@@ -3,15 +3,11 @@
   :url "https://github.com/weavejester/ns-tracker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.namespace "1.1.0"]
                  [org.clojure/java.classpath "1.0.0"]]
   :aliases
-  {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8:+1.9:+1.10" "test"]}
+  {"test-all" ["with-profile" "default:+1.10" "test"]}
   :profiles
   {:dev  {:dependencies [[commons-io "2.6"]]}
-   :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
-   :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
-   :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
-   :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
    :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}})
