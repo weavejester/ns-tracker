66ab006153bc8a0846d8f6c10c5844f548d8aac1
James Reeves <jreeves@weavejester.com>
2024-06-19 17:34:28 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.namespace "1.1.0"]
-                 [org.clojure/java.classpath "1.0.0"]]
+                 [org.clojure/tools.namespace "1.5.0"]
+                 [org.clojure/java.classpath "1.1.0"]]
   :aliases
   {"test-all" ["with-profile" "default:+1.10:+1.11:+1.12" "test"]}
   :profiles
-  {:dev  {:dependencies [[commons-io "2.6"]]}
+  {:dev  {:dependencies [[commons-io "2.16.1"]]}
    :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}
    :1.11 {:dependencies [[org.clojure/clojure "1.11.3"]]}
    :1.12 {:dependencies [[org.clojure/clojure "1.12.0-beta1"]]}})
